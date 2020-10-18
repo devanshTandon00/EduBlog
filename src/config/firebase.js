d45b816813fe0ac64ed5c9e3bac64e6017f34e08
nguyen-nhat <natt.lefevre@gmail.com>
2020-10-18 10:57:30 -0700

    🐛 Fix Firebase bug

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,17 +1,17 @@
-import firebase from 'firebase';
+import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
 
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyBs3yBcbQGQB28r3dIXwu26f9HzUg6rflQ',
-    authDomain: 'npath-blog.firebaseapp.com',
-    databaseURL: 'https://npath-blog.firebaseio.com',
-    projectId: 'npath-blog',
-    storageBucket: 'npath-blog.appspot.com',
-    messagingSenderId: '89871968991',
-    appId: '1:89871968991:web:01944962ed23d8813d176a',
-    measurementId: 'G-24GM5718RV'
+  apiKey: 'AIzaSyBs3yBcbQGQB28r3dIXwu26f9HzUg6rflQ',
+  authDomain: 'npath-blog.firebaseapp.com',
+  databaseURL: 'https://npath-blog.firebaseio.com',
+  projectId: 'npath-blog',
+  storageBucket: 'npath-blog.appspot.com',
+  messagingSenderId: '89871968991',
+  appId: '1:89871968991:web:01944962ed23d8813d176a',
+  measurementId: 'G-24GM5718RV'
 };
 
 firebase.initializeApp(firebaseConfig);
