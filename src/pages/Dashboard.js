1bb2c8aa4c5acf5507d319b0e4d007b90ef9a15d
Cameron Lau <cameron1998@gmail.com>
2020-11-21 16:40:45 -0800

    first iteration of login system

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
 import Navbar from "../Components/navbar";
 import Card2 from "../Components/card2";
-import { Link } from "react-router-dom";
-import { db } from "../config/firebase";
+import { Redirect, Link } from "react-router-dom";
+import firebase, { db } from "../config/firebase";
 
 export default class Dashboard extends Component {
-  state = { posts: null };
+  constructor() {
+    super();
+    this.state = {
+      posts: null,
+      signOut: false,
+      user: null,
+    };
+
+    this.handleSignOut = this.handleSignOut.bind(this);
+  }
+
+  currentUser = firebase.auth().currentUser;
 
   componentDidMount() {
     console.log("mounted");
@@ -22,20 +33,34 @@ export default class Dashboard extends Component {
       .catch((error) => console.error(error));
   }
 
+  handleSignOut(e) {
+    firebase.auth().signOut();
+    this.setState({ signOut: true });
+  }
+
   render() {
+    if (this.state.signOut) {
+      this.setState({ signOut: false });
+      return <Redirect to="/login" />;
+    }
+    if (this.currentUser == null) {
+      return <Redirect to="/login" />;
+    }
     return (
       <>
         <Navbar />
         <div className="card">
           <Link
-          to={"addPost/"}
-          style={{ float: "right", marginTop: 22, paddingRight: 30 }}
+            to={"addPost/"}
+            style={{ float: "right", marginTop: 22, paddingRight: 30 }}
           >
             <button className="button2"> Create Post </button>
           </Link>
           <h1 style={{ textAlign: "left", paddingLeft: 30 }}>
             Admin Dashboard
           </h1>
+          <h1>Welcome, {this.currentUser.email}</h1>
+          <button type="submit" onClick={this.handleSignOut}>Sign Out!</button>
           <h2 style={{ textAlign: "left", paddingLeft: 30 }}>Blog Archive</h2>
           <div className="container2">
             {this.state.posts &&
