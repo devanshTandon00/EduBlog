971130daff2031f58f46fe2d523a486e442f1d6f
Ngoc Doan <doan.ngoc678@gmail.com>
2020-12-03 15:35:54 -0800

    Changed UI of field entries for Edit Post Page

--- src/pages/editPost.js
+++ src/pages/editPost.js
@@ -56,13 +56,36 @@ export default function EditPost() {
     <>
       <Navbar />
       <div className="card3">
+        <svg
+          style={{
+            float: "left",
+            width: 45,
+            height: 45,
+            marginLeft: 30,
+            marginRight: 10,
+            marginTop: 18,
+          }}
+          class="svg-icon"
+          viewBox="0 0 20 20"
+        >
+          <path d="M12.443,9.672c0.203-0.496,0.329-1.052,0.329-1.652c0-1.969-1.241-3.565-2.772-3.565S7.228,6.051,7.228,8.02c0,0.599,0.126,1.156,0.33,1.652c-1.379,0.555-2.31,1.553-2.31,2.704c0,1.75,2.128,3.169,4.753,3.169c2.624,0,4.753-1.419,4.753-3.169C14.753,11.225,13.821,10.227,12.443,9.672z M10,5.247c1.094,0,1.98,1.242,1.98,2.773c0,1.531-0.887,2.772-1.98,2.772S8.02,9.551,8.02,8.02C8.02,6.489,8.906,5.247,10,5.247z M10,14.753c-2.187,0-3.96-1.063-3.96-2.377c0-0.854,0.757-1.596,1.885-2.015c0.508,0.745,1.245,1.224,2.076,1.224s1.567-0.479,2.076-1.224c1.127,0.418,1.885,1.162,1.885,2.015C13.961,13.689,12.188,14.753,10,14.753z M10,0.891c-5.031,0-9.109,4.079-9.109,9.109c0,5.031,4.079,9.109,9.109,9.109c5.031,0,9.109-4.078,9.109-9.109C19.109,4.969,15.031,0.891,10,0.891z M10,18.317c-4.593,0-8.317-3.725-8.317-8.317c0-4.593,3.724-8.317,8.317-8.317c4.593,0,8.317,3.724,8.317,8.317C18.317,14.593,14.593,18.317,10,18.317z"></path>
+        </svg>
         <Link
           to={"/dashboard"}
-          style={{ float: "right", marginTop: 22, paddingRight: 30 }}
+          style={{ float: "right", marginTop: 20, paddingRight: 30 }}
         >
           <button className="button2"> Back </button>
         </Link>
-        <h1 style={{ textAlign: "left", paddingLeft: 30 }}>Admin Dashboard</h1>
+        <h1
+          style={{
+            textAlign: "left",
+            paddingLeft: 30,
+            marginTop: 20,
+            fontSize: 35,
+          }}
+        >
+          Admin Dashboard
+        </h1>
         <h2 style={{ textAlign: "left", paddingLeft: 30 }}>Edit Post</h2>
 
         {/* <button onClick={handleSubmit}>Tester</button> */}
@@ -71,59 +94,58 @@ export default function EditPost() {
           onSubmit={handleSubmit}
         >
           <div className="fields">
-              <h3>Post Title:</h3>
-              <div class="entry">
-                <input
-                  type="text"
-                  name="postName"
-                  value={state.postName}
-                  onChange={handleChange}
-                  required={true}
-                ></input>
-              </div>
+            <h3>Post Title:</h3>
+            <div class="entry">
+              <input
+                type="text"
+                name="postName"
+                value={state.postName}
+                onChange={handleChange}
+                required={true}
+              ></input>
+            </div>
 
-              <h3>Post Content:</h3>
-              <div class="entry">
-                <input
-                  style={{ height: 500, width: 800 }}
-                  type="text"
-                  name="postContent"
-                  value={state.postContent}
-                  onChange={handleChange}
-                ></input>
-              </div>
+            <h3>Post Content:</h3>
+            <div class="entry">
+              <input
+                style={{ height: 500, width: 800 }}
+                type="text"
+                name="postContent"
+                value={state.postContent}
+                onChange={handleChange}
+              ></input>
+            </div>
 
-              <h3>Image URL:</h3>
-              <div class="entry">
-                <input
-                  type="text"
-                  name="image"
-                  value={state.image}
-                  onChange={handleChange}
-                ></input>
-              </div>
+            <h3>Image URL:</h3>
+            <div class="entry">
+              <input
+                type="text"
+                name="image"
+                value={state.image}
+                onChange={handleChange}
+              ></input>
+            </div>
 
-              <h3>Author Name:</h3>
-              <div class="entry">
-                <input
-                  type="text"
-                  name="authName"
-                  value={state.authName}
-                  onChange={handleChange}
-                ></input>
-              </div>
+            <h3>Author Name:</h3>
+            <div class="entry">
+              <input
+                type="text"
+                name="authName"
+                value={state.authName}
+                onChange={handleChange}
+              ></input>
+            </div>
 
-              <h3>Date:</h3>
-              <div class="entry">
-                <input
-                  type="text"
-                  name="date"
-                  value={state.date}
-                  onChange={handleChange}
-                  readOnly={true}
-                ></input>
-              </div>
-
+            <h3>Date:</h3>
+            <div class="entry">
+              <input
+                type="text"
+                name="date"
+                value={state.date}
+                onChange={handleChange}
+                readOnly={true}
+              ></input>
+            </div>
           </div>
           <button className="button" type="submit">
             Submit
